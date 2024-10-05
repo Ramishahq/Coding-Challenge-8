f7afe91428dd8a6b7f9ec91626c9ce81f33ea4d0
ramishahq <hoque2@usf.edu>
2024-10-05 01:37:10 -0400

    Handle bonuses in the department salary calculation

--- employee_management.js
+++ employee_management.js
@@ -14,7 +14,9 @@ getDetails() {
     
     }
 // Task 2 Create a Department Class
-class department {
+// Task 4 Handle Bonuses for Managers 
+// Adding task 4 to task 2 as instructed to add method to the department class.
+class Department {
     constructor(name) {
         this.name = name;
         this.employees = [];
@@ -27,6 +29,16 @@ class department {
 
 getDepartmentSalary() {
     return this.employees.reduce((total,employee)=> total + employee.salary,0);
+}
+
+// Method to calculate total salary including bonuses for managers
+calculateTotalSalaryWithBonus() {
+    return this.employees.reduce((total, employee) => {
+        if (employee instanceof Manager) {
+            return total + employee.salary + employee.bonus;
+        }
+        return total + employee.salary;
+    }, 0);
 }
 }
 // Task 3  
