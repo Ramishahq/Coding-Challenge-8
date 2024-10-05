dce7f634d97f45cc058df7340b2a23a25c4fdb54
ramishahq <hoque2@usf.edu>
2024-10-05 01:28:09 -0400

    "Create Manager class with inheritance from Employee"

--- employee_management.js
+++ employee_management.js
@@ -7,7 +7,7 @@ class Employee {constructor(name,salary,position,department) {
     this.department= department;
 }
 
-getEmployeeDetails() {
+getDetails() {
     return `The employee's name is ${this.name}. They work as ${this.position} in the ${this.department} department, and makes $${this.salary} salary.`;
     
       }
@@ -29,3 +29,19 @@ getDepartmentSalary() {
     return this.employees.reduce((total,employee)=> total + employee.salary,0);
 }
 }
+// Task 3  
+class Manager extends Employee {
+
+     constructor(name, salary, position, department, bonus) {
+    
+     super(name, salary, position, department); // Call the parent constructor
+    
+     this.bonus = bonus;
+    
+      }
+      getDetails() {
+        console.log(`${this.name} has $${this.salary} salary and works as ${this.position} in this ${this.department} department and has bonus of $$(this.bonus)`);
+
+    }
+}
+
